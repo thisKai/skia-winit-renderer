78cb84203ffe1ef1b20680ddd004523431f4de3b
Kai <thiskaiguy@gmail.com>
2023-02-10 17:24:50 +0000

    Make gl private

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
-pub mod gl {
+pub(crate) mod gl {
     #![allow(clippy::all)]
     include!(concat!(env!("OUT_DIR"), "/gl_bindings.rs"));
 
-    pub use Gles2 as Gl;
+    pub(crate) use Gles2 as Gl;
 }
 
 mod app;
