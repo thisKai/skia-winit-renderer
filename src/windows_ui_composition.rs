4c107a53e9c7a2ace8277da250fa46ef478a4e27
Kai Patrick <thiskaiguy@gmail.com>
2024-06-10 20:11:45 +0100

    Update skia-d3d12-swap-chain

--- src/windows_ui_composition.rs
+++ src/windows_ui_composition.rs
@@ -2,7 +2,10 @@ use provide_any::provide_any::{request_mut, Provider};
 
 use skia_d3d12_swap_chain::{WinCompBackend, WinCompSwapChain, WinCompTarget};
 use windows::{
-    Foundation::Numerics::{Vector2, Vector3},
+    Foundation::{
+        Numerics::{Vector2, Vector3},
+        TypedEventHandler,
+    },
     UI::Composition::{CompositionSurfaceBrush, SpriteVisual},
 };
 use winit::{
@@ -30,28 +33,52 @@ impl WindowsUiCompositionBackend {
     ) -> windows::core::Result<WindowsUiCompositionRenderer> {
         let size = window.inner_size();
         let target = WinCompTarget::with_window(&window)?;
+        target
+            .controller
+            .CommitNeeded(&TypedEventHandler::new(|sender, r| {
+                println!("Commit Needed");
+                Ok(())
+            }))
+            .unwrap();
 
         let swap_chain = self.0.create_swap_chain(size.width, size.height).unwrap();
 
         let surface = target.create_surface(&swap_chain).unwrap().unwrap();
         let brush = target
-            .compositor
+            .controller
+            .Compositor()
+            .unwrap()
             .CreateSurfaceBrushWithSurface(&surface)
             .unwrap();
         brush
-            .SetStretch(windows::UI::Composition::CompositionStretch::UniformToFill)
+            .SetStretch(windows::UI::Composition::CompositionStretch::None)
             .unwrap();
 
-        let visual = target.compositor.CreateSpriteVisual().unwrap();
-        visual.SetBrush(&brush).unwrap();
-        visual
-            .SetRelativeSizeAdjustment(Vector2 { X: 1.0, Y: 1.0 })
+        let visual = target
+            .controller
+            .Compositor()
+            .unwrap()
+            .CreateSpriteVisual()
             .unwrap();
+        visual.SetBrush(&brush).unwrap();
+
+        visual.SetOffset(Vector3 {
+            X: 0.0,
+            Y: 0.0,
+            Z: 0.0,
+        })?;
+        visual.SetSize(Vector2 {
+            X: size.width as _,
+            Y: size.height as _,
+        })?;
 
         target.desktop_window_target.SetRoot(&visual).unwrap();
 
+        target.controller.Commit()?;
+
         Ok(WindowsUiCompositionRenderer {
             target,
+            visual,
             brush,
             swap_chain,
         })
@@ -91,6 +118,7 @@ impl SkiaGraphicsBackend for WindowsUiCompositionBackend {
 
 pub struct WindowsUiCompositionRenderer {
     target: WinCompTarget,
+    visual: SpriteVisual,
     brush: CompositionSurfaceBrush,
     swap_chain: WinCompSwapChain,
 }
@@ -111,11 +139,27 @@ impl SkiaRender for WindowsUiCompositionRenderer {
 
     fn present(&mut self, env: &mut dyn Provider) {
         let env = request_mut::<WindowsUiCompositionBackend>(env).unwrap();
-        self.swap_chain.present(&mut env.0)
+        self.swap_chain.present(&mut env.0, &self.target).unwrap();
     }
 
     fn resize(&mut self, env: &mut dyn Provider, size: PhysicalSize<u32>, _: &Window) {
         let env = request_mut::<WindowsUiCompositionBackend>(env).unwrap();
-        self.swap_chain.resize(&mut env.0, size.width, size.height)
+
+        self.swap_chain
+            .resize(&mut env.0, &self.target, size.width, size.height);
+        self.visual
+            .SetOffset(Vector3 {
+                X: 0.0,
+                Y: 0.0,
+                Z: 0.0,
+            })
+            .unwrap();
+        self.visual
+            .SetSize(Vector2 {
+                X: size.width as _,
+                Y: size.height as _,
+            })
+            .unwrap();
+        self.target.controller.Commit().unwrap();
     }
 }
