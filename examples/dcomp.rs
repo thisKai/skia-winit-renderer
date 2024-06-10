104225a12d0b9713c76476495b4166f8e06042a7
Kai Patrick <thiskaiguy@gmail.com>
2024-06-10 17:48:21 +0100

    Remove old DirectComposition backend

--- examples/dcomp.rs
+++ examples/dcomp.rs
@@ -1,5 +1,7 @@
 use skia_safe::{colors, Paint};
-use skia_winit_renderer::dcomp::DCompWindowManager;
+use skia_winit_renderer::{
+    generic::WindowManager, dcomp::DirectCompositionBackend,
+};
 use winit::{
     event::{Event, WindowEvent},
     event_loop::EventLoopBuilder,
@@ -9,38 +11,36 @@ use winit::{
 fn main() {
     let event_loop = EventLoopBuilder::new().build().unwrap();
 
-    let mut window_manager = DCompWindowManager::new().unwrap();
+    let mut window_manager =
+        WindowManager::<DirectCompositionBackend>::new(&event_loop).unwrap();
 
     event_loop
         .run(|event, elwt| match event {
             Event::Resumed => {
                 window_manager
-                    .create_window(elwt, WindowBuilder::new().with_transparent(true))
+                    .create(elwt, WindowBuilder::new().with_transparent(true))
                     .unwrap();
             }
             Event::WindowEvent { window_id, event } => match event {
                 WindowEvent::CloseRequested => {
-                    window_manager.remove_window(&window_id);
+                    window_manager.remove(&window_id);
                     elwt.exit();
                 }
                 WindowEvent::RedrawRequested => {
-                    // window_manager
-                    //     .draw(&window_id, |canvas, window| {
-                    //         canvas.clear(colors::TRANSPARENT);
+                    window_manager.draw(&window_id, |canvas, window| {
+                        canvas.clear(colors::TRANSPARENT);
 
-                    //         let size = window.inner_size();
+                        let size = window.inner_size();
 
-                    //         canvas.draw_circle(
-                    //             ((size.width / 2) as i32, (size.height / 2) as i32),
-                    //             size.width.min(size.height) as f32 / 2.0,
-                    //             &Paint::new(colors::CYAN, None),
-                    //         );
-                    //     })
-                    //     .unwrap();
-                    window_manager.draw_handler(&window_id).unwrap();
+                        canvas.draw_circle(
+                            ((size.width / 2) as i32, (size.height / 2) as i32),
+                            size.width.min(size.height) as f32 / 2.0,
+                            &Paint::new(colors::CYAN, None),
+                        );
+                    });
                 }
                 WindowEvent::Resized(size) => {
-                    // window_manager.resize_window(&window_id, size).unwrap();
+                    window_manager.resize(&window_id, size);
                 }
                 _ => {}
             },
